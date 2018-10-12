2f1f1f58daa3889cdcd2d5835a0949426ff2ddd5
chenwu0 <chenwu0@aliyun.com>
2018-10-12 19:55:01 +0800

    转码处理

--- src/com/szit/arbitrate/client/entity/enumvo/AuthorityGroupEnum.java
+++ src/com/szit/arbitrate/client/entity/enumvo/AuthorityGroupEnum.java
@@ -1,29 +1,11 @@
 package com.szit.arbitrate.client.entity.enumvo;
 
 public enum AuthorityGroupEnum {
-	/**
-	 * 游客
-	 */
 	Visitor,
-	/**
-	 * 普通会员
-	 */
 	Normal,
-	/**
-	 * 认证会员
-	 */
 	Certificated,
-	/**
-	 * 调解分发中心
-	 */
 	MediationCenter,
-	/**
-	 * 调解机构
-	 */
 	MediationAgency,
-	/**
-	 * 调解员
-	 */
 	Mediator;
 
 }
